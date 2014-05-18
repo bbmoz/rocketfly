15fcf4c6f7cb10cd4165bbd6f3998e3aec083530
senatwj <senatwj@rose-hulman.edu>
2014-05-18 14:39:32 -0400

    simple menu screen

---
+++ Assets/Scripts/MenuScripts/MenuGameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuGameManager : MonoBehaviour {
+	
+	public GameObject player;
+	
+	/// <summary>
+	/// Spawns the player.
+	/// </summary>
+	private void SpawnPlayer() {
+		Transform t = (Instantiate(player, Vector3.zero, Quaternion.AngleAxis(90, Vector3.up)) as GameObject).transform;
+	}
+}
---
+++ Assets/Scripts/MenuScripts/MenuMouseHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMouseHandler : MonoBehaviour {
+
+	void OnMouseDown() {
+
+		if (this.name == "PlayBT") {
+
+			Application.LoadLevel ("Scene");
+			
+		} else if (this.name == "QuitBT") {
+			
+			Application.Quit();
+			
+		} else if (this.name == "OptionsBT") {
+			
+			// options button behavior
+			
+		}
+	}
+
+}
